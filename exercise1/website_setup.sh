95fa8b7e87b7c161b551b64ab19684b2761faac0
Bishal Rayamajhi <rayamajhibishal556@gmail.com>
2023-11-19 13:11:50 +0545

    udpated website setup script

--- exercise1/website_setup.sh
+++ exercise1/website_setup.sh
@@ -1,6 +1,8 @@
 cd /tmp/
+sudo rm -r 2137_barista_cafe
+sudo rm 2137_barista_cafe*.zip
 wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
-uzip 2137_barista_cafe.zip 
+uzip 2137_barista_cafe.zip
 cp -r 2137_barista_cafe/* /var/www/html
 sudo systemctl restart httpd
 
