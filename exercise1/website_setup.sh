2a1e7c6114ff8841278e6f6ce5b8ba325984e14c
Bishal Rayamajhi <rayamajhibishal556@gmail.com>
2023-11-19 13:14:47 +0545

    typo error fixed in website

--- exercise1/website_setup.sh
+++ exercise1/website_setup.sh
@@ -1,9 +1,9 @@
-cd /tmp/
+cd /tmp
 sudo rm -r 2137_barista_cafe
 sudo rm 2137_barista_cafe*.zip
 wget https://www.tooplate.com/zip-templates/2137_barista_cafe.zip
-uzip 2137_barista_cafe.zip
-cp -r 2137_barista_cafe/* /var/www/html
+unzip 2137_barista_cafe.zip
+cp -r 2137_barista_cafe/* /var/www/html/
 sudo systemctl restart httpd
 
 
