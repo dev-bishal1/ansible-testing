46c5e2f2e1ff541b2b12deb9835d3eb016f64e76
Bishal Rayamajhi <developerbishal01@gmail.com>
2023-12-10 21:57:55 +0545

    typo issue in api.sh file fixed

--- avendi-ansible/roles/avendi-api-roles/files/api.sh
+++ avendi-ansible/roles/avendi-api-roles/files/api.sh
@@ -1,3 +1,3 @@
 docker build -t avendi-api .
 
-docker container run --name avendi-api-container --port 3000:3000 -d avendi-api
+docker container run --name avendi-api-container -p 3000:3000 -d avendi-api
